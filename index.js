30e19c85b2261e57a949d9de97c5e7293ef721af
David Santucci <dsantucci@gnaritas.com>
2017-03-24 18:54:05 -0400

    Accept either JSON or base64 encoded body; Add primary key and foreign keys to output.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Datapackage = require("datapackage").Datapackage;
 const JSZip = require('jszip');
 
 
+var zipArchive = null;
+
 function sqlDataType (field) {
 	var types = {
 		"string":"varchar",
@@ -17,6 +19,10 @@ function sqlDataType (field) {
 	return field.type in types ? types[field.type] : "varchar";
 }
 
+function getArray (val) {
+	return Array.isArray(val) ? val : [val];
+}
+
 function getColumnDefs (schema) {
 	var defs = [];
 	schema.fields.forEach(function (field) {
@@ -24,14 +30,29 @@ function getColumnDefs (schema) {
 		defs.push(def);
 	});
 
+	if (schema.primaryKey) {
+		var cols = getArray(schema.primaryKey);
+		var format = (new Array(cols.length)).fill("`%s`").join(", ");
+		var args = [format].concat(cols);
+		defs.push("primary key ("+printf.apply({}, args)+")");
+	}
+
+	if (schema.foreignKeys) {
+		schema.foreignKeys.forEach(function (fk) {
+			var def = printf("foreign key (`%s`)", fk.fields);
+			def += printf("references `%s` (`%s`)", fk.reference.resource, fk.reference.fields);
+			defs.push(def);
+		});
+	}
+
 	return defs;
 }
 
 function getTableDef (resource) {
 	var output = "";
 	output += printf("create table `%s` (", resource.name);	
-	output += ("\n\t" + getColumnDefs(resource.descriptor.schema).join(",\n\t"));
-	output += ("\n);\n");
+	output += "\n\t" + getColumnDefs(resource.descriptor.schema).join(",\n\t");
+	output += "\n);\n";
 
 	return output;
 }
@@ -53,10 +74,42 @@ function parseBase64 (str) {
 				catch(err) {
 					console.log("Can't parse base 64!");
 					reject("Base 64 parse failure: "+err.message);
+					return;
 				}
 			}	
+			console.log("Parsed base64 body");
 			resolve(buf);
 	});	
+}
+
+function parseBody (body) {
+	zipArchive = null; // DS: This seems to hang around between invocations on AWS
+	try {
+		var data = JSON.parse(body);
+		console.log("Parsed JSON body");
+		return new Promise(function (resolve, reject) {
+			resolve(data);
+		});
+	}
+	catch (e) {
+		return parseBase64Body(body);
+	}
+}
+
+function parseBase64Body (body) {
+	
+	return parseBase64(body).then(
+		zipFile => JSZip.loadAsync(zipFile)		
+	).then(function (zip) {
+		console.log("Parsed ZIP archive");
+		zipArchive = zip;
+		var descriptor = zip.file("datapackage.json");
+		if (!descriptor) {
+			throw new Error("Can't find datapackage.json at archive root.");
+		}
+		return descriptor.async("string");
+	}).then(JSON.parse);
+
 }
 
 exports.handler = function (event, context, callback) {
@@ -72,16 +125,8 @@ exports.handler = function (event, context, callback) {
 		    },
 		});	
 	};		
-
-	parseBase64(event.body).then(
-		zipFile => JSZip.loadAsync(zipFile)		
-	).then(function (zip) {
-		var descriptor = zip.file("datapackage.json");
-		if (!descriptor) {
-			throw new Error("Can't find datapackage.json at archive root.");
-		}
-		return descriptor.async("string");
-	}).then(JSON.parse).then(		
+	
+	parseBody(event.body).then(		
 		data => new Datapackage(data)
 	).then(function (datapackage) {		
 		var output = "";
@@ -89,6 +134,10 @@ exports.handler = function (event, context, callback) {
 			output += getTableDef(resource);		
 		});
 		
+		if (zipArchive) {					
+			output += "\n -- Insert statements from ZIP archive data to go here.\n"
+		}
+
 		return output;
 
 	}).then(
