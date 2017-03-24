9c22b63346e0cddb9f262f116a56652ad32477f2
David Santucci <dsantucci@gnaritas.com>
2017-03-24 11:43:02 -0400

    Fall back to Buffer constructor for AWS node.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-require("buffer-v6-polyfill");
-
 const printf = require('printf');
 const Datapackage = require("datapackage").Datapackage;
 const JSZip = require('jszip');
@@ -38,45 +36,65 @@ function getTableDef (resource) {
 	return output;
 }
 
-exports.handler = function (event, context, callback) {
-	const done = (err, res) => callback(null, {
-	    statusCode: err ? '400' : '200',
-	    body: err ? err.message : res,
-	    headers: {
-	        'Content-Type': 'text/plain',
-	    },
-	});	
-	
-	var output = "";
-	
-	var zipFile = Buffer.from(event.body, 'base64');	
+function oldParseBase64 (str) {
+	return new Buffer(str, 'base64');
+}
 
-	JSZip.loadAsync(zipFile).then(function (zip) {
+function parseBase64 (str) {
+	return new Promise(function (resolve, reject) {
+		var buf;
+			try {
+				buf = Buffer.from(str, 'base64');
+			}
+			catch (err) {
+				try {
+					buf = oldParseBase64(str);
+				}
+				catch(err) {
+					console.log("Can't parse base 64!");
+					reject("Base 64 parse failure: "+err.message);
+				}
+			}	
+			resolve(buf);
+	});	
+}
+
+exports.handler = function (event, context, callback) {
+	const done = (err, res) => {
+		if(err) {
+			console.log("Error: "+err.message);
+		}
+		callback(null, {
+		    statusCode: err ? '400' : '200',
+		    body: err ? err.message : res,
+		    headers: {
+		        'Content-Type': 'text/plain',
+		    },
+		});	
+	};		
+
+	parseBase64(event.body).then(
+		zipFile => JSZip.loadAsync(zipFile)		
+	).then(function (zip) {
 		var descriptor = zip.file("datapackage.json");
 		if (!descriptor) {
 			throw new Error("Can't find datapackage.json at archive root.");
 		}
 		return descriptor.async("string");
-	}).then(JSON.parse).then(function (data) {			
-		return new Datapackage(data);
-	}).then(function (datapackage) {
-		console.log("Found "+datapackage.resources.length+" resources");
-			
+	}).then(JSON.parse).then(		
+		data => new Datapackage(data)
+	).then(function (datapackage) {		
+		var output = "";
 		datapackage.resources.forEach(function (resource) {		
 			output += getTableDef(resource);		
 		});
 		
 		return output;
 
-	}).then(function (ouptut) {
-		done(null, output);
-	}).catch(function (err) {
-		console.log("Error: "+err);
-		done(new Error(err));
-	});
-		
+	}).then(
+		output => done(null, output)
+	).catch(		
+		err => done(new Error(err))
+	);		
 };
-
-
-
 
